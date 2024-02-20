b74d3d63424a0841540a78d3e272c69ab0801827
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-02-20 14:54:05 -0500

    did some stuff but forgot what it was

---
+++ app/src/main/java/com/example/williamderocco_assignment4_expensetracker/AddExpenseFragment.kt
@@ -0,0 +1,36 @@
+package com.example.williamderocco_assignment4_expensetracker
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.williamderocco_assignment4_expensetracker.databinding.FragmentAddExpenseBinding
+
+private const val TAG = "AddExpenseFragment"
+
+class AddExpenseFragment : Fragment() {
+    private var _binding: FragmentAddExpenseBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =
+            FragmentAddExpenseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
